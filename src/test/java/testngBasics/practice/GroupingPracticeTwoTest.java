e2f44799511adb729ecd06eb0ba7ec723c43bdc8
Rezart1 <rezartbe@gmail.com>
2024-04-21 11:27:48 -0400

    TestBase class added. Order of execution of annotations

---
+++ src/test/java/testngBasics/practice/GroupingPracticeTwoTest.java
@@ -0,0 +1,15 @@
+package testngBasics.practice;
+
+import org.testng.annotations.Test;
+
+public class GroupingPracticeTwoTest {
+    @Test(groups = {"regression", "smoke"})
+    public void test_case_seven(){
+        System.out.println("Test case Seven running..");
+    }
+
+    @Test(groups = {"regression"})
+    public void test_case_eight(){
+        System.out.println("Test case Eight running..");
+    }
+}
